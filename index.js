3526953c2a3bdbc864256618d53e03b021bb3dbb
web-joyanta <web.joyanta@gmail.com>
2025-05-31 11:13:13 +0600

    new campaigns post method insert data

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res)=>{
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://crowdFunding:f9qyM87LqWSngGbO@cluster0.z1ypfcb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        const database = client.db("crowdFundingDB");
+        const campaignsCollection = database.collection("campaigns");
+
+        app.post("/campaigns", async (req, res) => {
+            const newCampaign = req.body;
+            const result = await campaignsCollection.insertOne(newCampaign);
+            res.send(result);
+        })
+
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get("/", (req, res) => {
     res.send("Server in Running...");
 })
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Server in Running on Port ${port}`);
 });
