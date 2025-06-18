852de5a825d9f584f1816f6f8116f4a05be75503
web-joyanta <web.joyanta@gmail.com>
2025-06-18 20:43:26 +0600

    donate data added to database donateCollection

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = "mongodb+srv://crowdFunding:f9qyM87LqWSngGbO@cluster0.z1ypfcb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -23,10 +23,38 @@ async function run() {
     try {
         const database = client.db("crowdFundingDB");
         const campaignsCollection = database.collection("campaigns");
+        const donatedCollection = database.collection("donatedCollection");
 
+        app.get("/campaigns", async (req, res) => {
+            const query = campaignsCollection.find();
+            const result = await query.toArray();
+            res.send(result);
+        })
+        app.get("/campaign/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await campaignsCollection.findOne(query);
+            res.send(result);
+        });
+        app.get("/donate/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await donatedCollection.findOne(query);
+            res.send(result);
+        });
+        app.get("/donateds", async (req, res)=>{
+            const query = donatedCollection.find();
+            const result = await query.toArray();
+            res.send(result);
+        })
         app.post("/campaigns", async (req, res) => {
             const newCampaign = req.body;
             const result = await campaignsCollection.insertOne(newCampaign);
+            res.send(result);
+        });
+        app.post("/donateds", async (req, res)=>{
+            const newDonate = req.body;
+            const result = await donatedCollection.insertOne(newDonate);
             res.send(result);
         })
 
